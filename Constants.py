4635baef1eb401d196058680b0e9aa304c0de43f
chidubem ogbuefi <chidubem.ogbuefi@wemabank.com>
2023-08-10 12:53:55 +0100

    gunicorn fix

--- Constants.py
+++ Constants.py
@@ -2,12 +2,17 @@ btc_address="bc1qtnl9yqs24mumzcn0267serva7jmq8eu5sxvn93"
 
 API_KEY="5765524950:AAGxczslyNgjAX6HyDB5qxec8hOwK3MxzOg"
 usdt_trc20="TRxiMikr2nrvrLfsDzQ2nJw48d3bxpshDJ" 
-test_key="sk_live_083c012b34f37b27be2f8613027b6b7dd854ce18"
+# test_key="sk_live_083c012b34f37b27be2f8613027b6b7dd854ce18"
+test_key='sk_test_e00c04149ff98cab4ce305a82f66deb78ac9f60e'
 twoweekssub='https://paystack.com/pay/inu5nupv5c'
-realonemonthsub='https://paystack.com/pay/uhkpzv8hqc'
+# onemonthsub='https://paystack.com/pay/uhkpzv8hqc'
+
+# test
+onemonthsub='https://paystack.com/pay/durtx74dcf'
+
 lifetimesub='https://paystack.com/pay/lnp166jo48'
 # test url
-onemonthsub='https://paystack.com/pay/uhkpzv8hqc'
+
 # db
 host="containers-us-west-126.railway.app"
 port=6927
