2a55677cb7237cf8e0e0e7952a65a96ba27d8fb3
chidubem ogbuefi <chidubem.ogbuefi@wemabank.com>
2023-08-10 22:04:32 +0100

    live bot

--- Constants.py
+++ Constants.py
@@ -1,14 +1,17 @@
 btc_address="bc1qtnl9yqs24mumzcn0267serva7jmq8eu5sxvn93"
 
-API_KEY="5765524950:AAGxczslyNgjAX6HyDB5qxec8hOwK3MxzOg"
+# API_KEY="5765524950:AAGxczslyNgjAX6HyDB5qxec8hOwK3MxzOg"
+
+# livebot
+API_KEY="6008220080:AAFKMhDPKqqpDjNjU-oMGTA7ulJtv-kQyIw"
 usdt_trc20="TRxiMikr2nrvrLfsDzQ2nJw48d3bxpshDJ" 
-# test_key="sk_live_083c012b34f37b27be2f8613027b6b7dd854ce18"
-test_key='sk_test_e00c04149ff98cab4ce305a82f66deb78ac9f60e'
+test_key="sk_live_083c012b34f37b27be2f8613027b6b7dd854ce18"
+# test_key='sk_test_e00c04149ff98cab4ce305a82f66deb78ac9f60e'
 twoweekssub='https://paystack.com/pay/inu5nupv5c'
-# onemonthsub='https://paystack.com/pay/uhkpzv8hqc'
+onemonthsub='https://paystack.com/pay/uhkpzv8hqc'
 
 # test
-onemonthsub='https://paystack.com/pay/durtx74dcf'
+# onemonthsub='https://paystack.com/pay/durtx74dcf'
 
 lifetimesub='https://paystack.com/pay/lnp166jo48'
 # test url
